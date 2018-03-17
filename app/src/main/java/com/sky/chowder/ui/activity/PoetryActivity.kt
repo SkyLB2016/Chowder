7c503dd29f6c53f800c1baa7debbeef488e90626
libin <1136096189@qq.com>
2018-03-17 22:39:33 +0800

    诗词文赋

--- app/src/main/java/com/sky/chowder/ui/activity/PoetryActivity.kt
+++ app/src/main/java/com/sky/chowder/ui/activity/PoetryActivity.kt
@@ -1,11 +1,11 @@
 package com.sky.chowder.ui.activity
 
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
 import com.sky.base.BaseNoPActivity
 import com.sky.chowder.R
-import com.sky.utils.LogUtils
 import kotlinx.android.synthetic.main.activity_poetry.*
 
 /**
@@ -14,10 +14,12 @@ import kotlinx.android.synthetic.main.activity_poetry.*
 class PoetryActivity : BaseNoPActivity(), View.OnClickListener {
     override fun getLayoutResId(): Int = R.layout.activity_poetry
     override fun initialize() {
-        val method = arrayListOf("千字文"
-                , "道德经"
+        val method = arrayListOf("道德经"
+                , "千字文"
                 , "三字经"
                 , "弟子规"
+                , "沁园春雪"
+                , "陋室铭"
         )
         for (i in method) {
             val tvText = LayoutInflater.from(this).inflate(R.layout.tv, flow, false) as TextView
@@ -31,15 +33,18 @@ class PoetryActivity : BaseNoPActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+        tvDisplay.gravity = Gravity.CENTER
         tvDisplay.text = getText(when (v?.tag) {
+            "道德经" -> {
+                tvDisplay.gravity = Gravity.LEFT
+                R.string.daodejing
+            }
             "千字文" -> R.string.qianziwen
-            "道德经" -> R.string.daodejing
             "三字经" -> R.string.sanzijing
             "弟子规" -> R.string.dizigui
+            "沁园春雪" -> R.string.xue
+            "陋室铭" -> R.string.loushiming
             else -> 0
         })
-
-
-        LogUtils.i("${tvDisplay.lineCount * 12}")
     }
 }
