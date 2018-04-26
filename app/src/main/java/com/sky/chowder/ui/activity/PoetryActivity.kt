6a9a38d52ddaa143ae1054fde26fc1442b5a2bb7
李彬 <1136096189@qq.com>
2018-04-26 17:53:17 +0800

    4.26 poetry阅读更改完成

--- app/src/main/java/com/sky/chowder/ui/activity/PoetryActivity.kt
+++ app/src/main/java/com/sky/chowder/ui/activity/PoetryActivity.kt
@@ -1,31 +1,52 @@
 package com.sky.chowder.ui.activity
 
 import android.graphics.RectF
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
+import com.sky.adapter.RecyclerAdapter
+import com.sky.adapter.RecyclerHolder
 import com.sky.base.BaseNoPActivity
 import com.sky.chowder.R
+import com.sky.chowder.model.ChapterEntity
 import com.sky.chowder.ui.pop.CatalogPop
-import com.sky.utils.LogUtils
 import com.sky.utils.ScreenUtils
 import com.sky.widget.BaseTitle
 import kotlinx.android.synthetic.main.activity_poetry.*
+import kotlinx.android.synthetic.main.item_tvdisplay.view.*
 import java.util.*
 
 /**
  * Created by SKY on 2018/3/16.
  */
 class PoetryActivity : BaseNoPActivity(), View.OnClickListener {
-
+    var gravity = Gravity.LEFT
+    var adapter: RecyclerAdapter<ChapterEntity>? = null
     private val resId = ArrayList<Int>()
     override fun getLayoutResId(): Int = R.layout.activity_poetry
     override fun initialize() {
         baseTitle.setLeftButton(R.mipmap.ic_menu)
         baseTitle.onLeftImgClick = BaseTitle.OnClickListener { sliding.toggleMenu() }
         baseTitle.setRightText("3.16建")
+        adapter = object : RecyclerAdapter<ChapterEntity>(R.layout.item_tvdisplay) {
+            override fun onAchieveHolder(holder: RecyclerHolder?, position: Int) {
+                with(holder!!.itemView) {
+                    tvDisplay.text = getDatas()[position].content
+                    tvDisplay.gravity = gravity
+                    if (position == 0) {
+                        tvDisplay.setPadding(resources.getDimensionPixelSize(R.dimen.wh_32),
+                                resources.getDimensionPixelSize(R.dimen.wh_16),
+                                resources.getDimensionPixelSize(R.dimen.wh_32),
+                                resources.getDimensionPixelSize(R.dimen.wh_4))
+                    }
+                }
+            }
+        }
+        recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recycler.adapter = adapter
 
         val poetry = resources.getStringArray(R.array.poetry)!!
         var tv: TextView
@@ -42,115 +63,99 @@ class PoetryActivity : BaseNoPActivity(), View.OnClickListener {
             resId.add(resources.getIdentifier(array[1], "string", packageName))
         }
 
-        tvDisplay.text = getString(resId[3]).replace(" ", "")
-        tvDisplay.gravity = Gravity.CENTER
-        setToolbarTitle(tvDisplay.text.lines()[0])
+
+        val text = getString(resId[3]).replace(" ", "")
+        gravity = Gravity.CENTER
+        setToolbarTitle(text.lines()[0])
         llBottomBar.visibility = View.GONE
         registerOnClick(tvLast, tvCatalog, tvNext)
+        getCatalog(text)
     }
 
     override fun registerView(v: View) = v.setOnClickListener(catalog)
 
     override fun onClick(v: View?) {
-        tvDisplay.gravity = when (v?.id) {
+        gravity = when (v?.id) {
             in 2..5 -> Gravity.CENTER
             else -> Gravity.LEFT
         }
-        tvDisplay.text = getString(resId[v!!.id]).replace(" ", "")
-        setToolbarTitle(tvDisplay.text.lines()[0])
-        LogUtils.i("总行数==${tvDisplay.lineCount}")
+        val text = getString(resId[v!!.id]).replace(" ", "")
+        setToolbarTitle(text.lines()[0])
+        getCatalog(text)
     }
 
     private val catalog = View.OnClickListener { v ->
         when (v?.id) {
-            R.id.tvLast -> moveToLine("●三十")
-            R.id.tvCatalog -> getCatalog()
+            R.id.tvLast -> upToChapter()
+            R.id.tvCatalog -> showCatalogPop(adapter?.datas!!)
             R.id.tvNext -> nextChapter()
         }
     }
 
     private fun nextChapter() {
-        val yy = scroll.scrollY
-        val line = (yy - tvDisplay.paddingTop) / tvDisplay.lineHeight
-        val layout = tvDisplay.layout
-        val start = layout.getLineStart(line+1)
-        val end = layout.getLineEnd(line+1)
+        val layoutManager = recycler.layoutManager as LinearLayoutManager
+        val position = layoutManager.findFirstVisibleItemPosition()
+        layoutManager.scrollToPositionWithOffset(position + 1, 0)
+        layoutManager.stackFromEnd = true
+    }
 
-        val article = tvDisplay.text
-        val text = article.substring(start, end)
+    private fun upToChapter() {
+        val layoutManager = recycler.layoutManager as LinearLayoutManager
+        val position = layoutManager.findFirstVisibleItemPosition()
+        recycler.scrollToPosition(position - 1)
+    }
 
-        val list = article.lines()
-        var position = 0
-        for (i in list.indices) {
-            if (list[i].contains(text))
-                position = i
+    private fun moveToChapter(position: Int) {
+        val layoutManager = recycler.layoutManager as LinearLayoutManager
+        layoutManager.scrollToPositionWithOffset(position, 0)
+        layoutManager.stackFromEnd = true
+    }
+
+    private fun getCatalog(text: String) {
+        val list = text.lines()
+        val catalog = ArrayList<ChapterEntity>()
+        var chapter = ChapterEntity()
+        chapter.chapter = list[0]
+        for (i in list) {
+            if (i.startsWith("第") && i.endsWith("章")
+                    || i.startsWith("卷")
+                    || i.contains("第") && !i.contains("。")) {
+                chapter.content.setLength(chapter.content.length-1)
+                catalog.add(chapter)
+                chapter = ChapterEntity()
+                chapter.chapter = i
+            } else if (i.contains(".")) {
+                chapter.content.setLength(chapter.content.length-1)
+                catalog.add(chapter)
+                chapter = ChapterEntity()
+                chapter.chapter = i.substring(2, i.indexOf("."))
+            }
+            chapter.content.append("$i\n")
         }
-
-        var catalog = ""
-        //筛选章节第**章
-        for (i in position until list.size) {
-            val text = list[i]
-            LogUtils.i("==$text")
-
-            if (catalog.isNotEmpty()) break
-            if (list[i].startsWith("第") && list[i].endsWith("章")) catalog = list[i]
-            //筛选章节卷
-            if (catalog.isNotEmpty()) break
-            if (list[i].startsWith("卷")) catalog = list[i]
-            //筛选章节第，结尾不含标点
-            if (catalog.isNotEmpty()) break
-            if (list[i].contains("第") && !list[i].contains("。")) catalog = list[i]
-            //筛选章节
-            if (catalog.isNotEmpty()) break
-            if (list[i].contains(".")) catalog = list[i]
-            //筛选章节10句为一章
+        var sign = -1
+        if (catalog.isEmpty() && list.size > 30) {
+            chapter.content = StringBuilder()
+            for (i in list.indices) {
+                if (sign === -1 && list[i].contains("，")) sign = i % 10
+                if (i % 10 === sign) {
+                    chapter.content.setLength(chapter.content.length - 1)
+                    catalog.add(chapter)
+                    chapter = ChapterEntity()
+                    chapter.chapter = list[i]
+                }
+                chapter.content.append("${list[i]}\n")
+            }
         }
-        if (catalog.isNotEmpty()) moveToLine(catalog) else showToast("已是最后一章")
+        chapter.content.setLength(chapter.content.length - 1)
+        catalog.add(chapter)
+        adapter?.datas = catalog
     }
 
-    private fun moveToLine(sign: String) {
-        val text = tvDisplay.text
-        val index = text.indexOf(sign)
-        tvDisplay.text = text.substring(0, index)
-        val line = tvDisplay.lineCount
-        tvDisplay.text = text
-        moveToLine(line - 1)
-    }
-
-    private fun moveToLine(line: Int) {
-        scroll?.scrollTo(0, tvDisplay.lineHeight * line + tvDisplay.paddingTop)
-//        val layout = tvDisplay.layout
-//        val rect = Rect()
-//        layout?.getLineBounds(line, rect)
-//        scroll?.scrollTo(0, rect.height() * line + tvDisplay.paddingTop)
-    }
-
-    private fun getCatalog() {
-        val list = tvDisplay.text.lines()
-        val catalog = ArrayList<String>()
-        //筛选章节第**章
-        for (i in list) if (i.startsWith("第") && i.endsWith("章")) catalog.add(i)
-        //筛选章节卷
-        if (catalog.isEmpty()) for (i in list) if (i.startsWith("卷")) catalog.add(i)
-        //筛选章节第，结尾不含标点
-        if (catalog.isEmpty()) for (i in list) if (i.contains("第") && !i.contains("。")) catalog.add(i)
-        //筛选章节
-        if (catalog.isEmpty()) for (i in list) if (i.contains(".")) catalog.add(i.substring(2, i.indexOf(".")))
-        //筛选章节10句为一章
-        var sign = 0
-        if (catalog.isEmpty() && list.size > 30) for (i in list.indices) {
-            if (sign === 0 && list[i].contains("，")) sign = i % 10
-            if (i % 10 === sign) catalog.add(list[i])
-        }
-        if (catalog.isNotEmpty()) showCatalogPop(catalog)
-        else showToast("无目录")
-    }
-
-    private fun showCatalogPop(floders: List<String>) {
+    private fun showCatalogPop(floders: List<ChapterEntity>) {
         val floderPop = CatalogPop(LayoutInflater.from(this).inflate(R.layout.include_recycler, null),
                 ScreenUtils.getWidthPX(this), (ScreenUtils.getHeightPX(this) * 0.7).toInt())
         floderPop?.datas = floders
-        floderPop?.setOnItemClickListener { _, position -> moveToLine(floders[position]) }
+        floderPop?.setOnItemClickListener { _, position -> moveToChapter(position) }
         floderPop?.showAtLocation(window.decorView, Gravity.CENTER, 0, 0)
     }
 
@@ -184,12 +189,6 @@ class PoetryActivity : BaseNoPActivity(), View.OnClickListener {
             llBottomBar.visibility = View.GONE
             return
         }
-
-        val yy = scroll.scrollY
-        val line = (yy - tvDisplay.paddingTop) / tvDisplay.lineHeight
-        setObject("line", line)
-        setObject("scrollX", scroll.scrollY)
-
     }
 
 //    override fun onRestart() {
--- app/src/main/java/com/sky/chowder/ui/pop/CatalogPop.kt
+++ app/src/main/java/com/sky/chowder/ui/pop/CatalogPop.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.sky.adapter.RecyclerAdapter
 import com.sky.adapter.RecyclerHolder
 import com.sky.chowder.R
+import com.sky.chowder.model.ChapterEntity
 import com.sky.widget.BasePop
 import kotlinx.android.synthetic.main.item_tv.view.*
 
@@ -12,18 +13,18 @@ import kotlinx.android.synthetic.main.item_tv.view.*
  * Created by SKY on 2016/1/11 13:14.
  * 文件夹pop
  */
-class CatalogPop(view: View, width: Int, height: Int) : BasePop<String>(view, width, height) {
+class CatalogPop(view: View, width: Int, height: Int) : BasePop<ChapterEntity>(view, width, height) {
     private var recycle: RecyclerView? = null
 
-    private var adapter: RecyclerAdapter<String>? = null
+    private var adapter: RecyclerAdapter<ChapterEntity>? = null
 
     override fun initView() {
         super.initView()
         recycle = view.findViewById(R.id.recycler)
-        adapter = object : RecyclerAdapter<String>(R.layout.item_tv) {
+        adapter = object : RecyclerAdapter<ChapterEntity>(R.layout.item_tv) {
             override fun onAchieveHolder(holder: RecyclerHolder, position: Int) {
                 with(holder!!.itemView) {
-                    tv.text = datas[position]
+                    tv.text = datas[position].chapter
                 }
             }
         }
