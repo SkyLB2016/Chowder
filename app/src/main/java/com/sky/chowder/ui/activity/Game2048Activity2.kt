f0f50b1f978c8d2c7a92e20d6d434de8f4904e24
李彬 <1136096189@qq.com>
2018-04-28 18:17:38 +0800

    4.28 准备开始

--- app/src/main/java/com/sky/chowder/ui/activity/Game2048Activity2.kt
+++ app/src/main/java/com/sky/chowder/ui/activity/Game2048Activity2.kt
@@ -1,7 +1,6 @@
 package com.sky.chowder.ui.activity
 
 import android.view.MotionEvent
-import com.sky.adapter.RecyclerAdapter
 import com.sky.base.BasePActivity
 import com.sky.chowder.R
 import com.sky.chowder.ui.presenter.GameP
@@ -11,13 +10,9 @@ import kotlinx.android.synthetic.main.activity_game.*
  * Created by SKY on 2018/4/20 11:17.
  */
 class Game2048Activity2 : BasePActivity<GameP>() {
-    var adapter: RecyclerAdapter<Int>? = null
-
     override fun getLayoutResId(): Int = R.layout.activity_game
     override fun creatPresenter(): GameP = GameP(this)
-
-    override fun initialize() {
-    }
+    override fun initialize() = Unit
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         game.onTouchEvent(event!!)
