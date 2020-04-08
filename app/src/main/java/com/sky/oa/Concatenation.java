5514c842514d5059c5576222f195687dadf2159a
libin <1136096189@qq.com>
2020-04-08 22:34:29 +0800

    修改

--- app/src/main/java/com/sky/oa/Concatenation.java
+++ app/src/main/java/com/sky/oa/Concatenation.java
@@ -1,11 +1,11 @@
 package com.sky.oa;
 
 public class Concatenation {
-  public static void main(String[] args) {
-      String userName = "Andy";
-      String age = "24";
-      String job = "Developer";
-      String info = userName + age + job;
-      System.out.println(info);
-  }
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        int c = a + b;
+        System.out.println(c);
+    }
 }
